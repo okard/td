22be033eedc759810d720c9a6f41dc2e005d9f89
okard <no@email.org>
2010-06-22 21:20:23 +0200

    small clean up

--- src/model/Map.cpp
+++ src/model/Map.cpp
@@ -60,45 +60,15 @@ void Map::Load(std::string fileName)
     imp.layerList.resize(2);
     imp.layerList[0].worldSize = 1500;
     imp.layerList[0].textureNames.push_back(conf.getSetting("WorldTexture"));
-    imp.layerList[1].worldSize = 50;
-    imp.layerList[1].textureNames.push_back(conf.getSetting("DetailTexture"));
+    //imp.layerList[1].worldSize = 50;
+    //imp.layerList[1].textureNames.push_back(conf.getSetting("DetailTexture"));
     
     
     //set up terrain
     mRtsEnv->getTerrain()->prepare(imp);
     mRtsEnv->getTerrain()->load();
     
-    /*
-    Ogre::Terrain* mTerrain = mRtsEnv->getTerrain();
-    Ogre::TerrainLayerBlendMap* blendMap0 = mTerrain->getLayerBlendMap(1);
-    //Ogre::TerrainLayerBlendMap* blendMap1 = mTerrain->getLayerBlendMap(2);
-    Ogre::Real minHeight0 = 70;
-    Ogre::Real fadeDist0 = 40;
-    Ogre::Real minHeight1 = 70;
-    Ogre::Real fadeDist1 = 15;
-    float* pBlend0 = blendMap0->getBlendPointer();
-    //float* pBlend1 = blendMap1->getBlendPointer();
-    for (Ogre::uint16 y = 0; y < mTerrain->getLayerBlendMapSize(); ++y)
-    {
-        for (Ogre::uint16 x = 0; x < mTerrain->getLayerBlendMapSize(); ++x)
-        {
-            Ogre::Real tx, ty;
-
-            blendMap0->convertImageToTerrainSpace(x, y, &tx, &ty);
-            Ogre::Real height = mTerrain->getHeightAtTerrainPosition(tx, ty);
-            Ogre::Real val = (height - minHeight0) / fadeDist0;
-            val = Ogre::Math::Clamp(val, (Ogre::Real)0, (Ogre::Real)1);
-
-            val = (height - minHeight1) / fadeDist1;
-            val = Ogre::Math::Clamp(val, (Ogre::Real)0, (Ogre::Real)1);
-            //*pBlend1++ = val * 255;
-        }
-    }
-    blendMap0->dirty();
-    //blendMap1->dirty();
-    blendMap0->update();
-    //blendMap1->update();
-    //*/
+    //Blend Map on/off for places possible to build?
     
     
     //set limits for camera
